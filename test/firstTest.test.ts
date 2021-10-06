7f95bf4ed5953c86a6d9f6c060095ac103116720
MiguelML Laptop <miguel.munoz.lorente.14@gmail.com>
2021-10-06 12:41:47 +0200

    Setup of the testing tool

---
+++ test/firstTest.test.ts
@@ -0,0 +1,14 @@
+import {suite, test} from "@testdeck/mocha";
+//import {mock, instance} from "ts-mockito";
+import * as _chai from "chai";
+
+_chai.should();
+
+@suite class firstTest {
+    private numberOne = 1;
+    
+
+    @test 'numberOne should be equal to 1'() {
+        _chai.expect(this.numberOne === 1);
+    }
+}
